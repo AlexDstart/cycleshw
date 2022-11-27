da48682066c9a0c9bc767ebe577967cbb7185694
AlexWorkPC <demyanalex@gmail.com>
2022-11-27 19:17:53 +0300

    задание 3-4

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,20 @@ public class Main {
                     System.out.println(" Итерация цикла " + i);
 
             }
+        System.out.println(" Задание 2 ");
                 //task 2
         for (int i = 10; i>=1 ; i--) {
             System.out.println(" Итерация цикла " + i);
+        }
+        // task 3
+        System.out.println(" Задание 3 ");
+        for (int i = 0; i < 17; i=i+2) {
+            System.out.println(i);
+        }
+        //task 4
+        System.out.println(" Задание 4 ");
+        for (int i = 10; i >=-10 ; i--) {
+            System.out.println(i);
         }
         }
     }
